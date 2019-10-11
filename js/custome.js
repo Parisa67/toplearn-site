846064009736c7f6a4cb5d221e255c495fa33f90
Parisa Mohamadi <parisamohamadi92@gmail.com>
2019-10-11 22:51:54 +0330

    responsive header & article

--- js/custome.js
+++ js/custome.js
@@ -1,5 +1,33 @@
-$('.counter').counterUp({
-    delay: 10,
-    time: 3000
+$(document).ready(function() {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 3000
+    });
+    new WOW().init();
+
+    var headerMenu = $("#header-top-menu");
+    var middleMenu = $("#header-middle-menu");
+    var bottomMenu = $("#header-bottom-menu");
+
+    $("#toggle-header-top").click(function(e) {
+
+        middleMenu.slideUp();
+        bottomMenu.slideUp();
+        headerMenu.slideToggle();
+
+    });
+    $("#toggle-header-middle").click(function(e) {
+        bottomMenu.slideUp();
+        headerMenu.slideUp();
+        middleMenu.slideToggle();
+
+    });
+
+    $("#toggle-header-bottom").click(function(e) {
+        headerMenu.slideUp();
+        middleMenu.slideUp();
+        bottomMenu.slideToggle();
+
+    });
+
 });
-new WOW().init();
